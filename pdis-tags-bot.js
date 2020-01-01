e6efff388b8ccf63ba9be6d9bbf7ce5b261eed2c
Mark Dai <mark19891107@users.noreply.github.com>
2020-01-01 23:09:18 +0800

    Updating pdis-tags-bot.js

--- pdis-tags-bot.js
+++ pdis-tags-bot.js
@@ -1,6 +1,5 @@
 var axios = require('axios');
-var async = require('asyncawait/async');
-var await = require('asyncawait/await');
+var Promise = require('es6-promise').Promise;
 var tags;
 var new_tags;
 var topic;
@@ -12,37 +11,38 @@ var apikey;
 var apiuser;
 
 module.exports = function (context, cb) {
+    console.log(context);
     apikey = context.secrets.apikey;
     apiuser = context.secrets.apiuser;
-    topic = context.data.topic;
+    topic = context.body.topic;
     topic_id = topic.id;
     topic_slug = topic.slug;
     topic_tags = topic.tags.slice();
     new_tags = topic.tags.slice();
 
-    go(context).then((result) => {
+    go().then((result) => {
         cb(null, result);
-    })
+    });
 
 };
 
-var go = async(function (context, cb) {
+var go = async function () {
 
-    await(get_tags());
+    await get_tags();
 
     // add tags by match topic title
-    new_tags = check_title(new_tags, topic);
-    
+    new_tags = check_title(new_tags);
+
     // add tags by match sayit content
-    links = await(get_sayit_link(topic));
-    contents = await(get_sayit_content(links));
+    let links = await get_sayit_link();
+    let contents = await get_sayit_content(links);
     new_tags = check_sayit_content(new_tags, contents);
-    
+
     // update new tags to discourse
-    result = await(update_discourse(new_tags));
+    let result = await update_discourse(new_tags);
     return result;
 
-});
+};
 
 function get_tags(){
     return axios.get("https://raw.githubusercontent.com/PDIS/discourse-tagger/master/tags.json")
@@ -54,12 +54,12 @@ function get_tags(){
 function check_sayit_content(old_tags, contents) {
 
     new_tags = old_tags.slice();
-    if(contents.length>0)
+    if(contents.length > 0)
     {
-        all_content = contents.reduce((all, content) => all + content);
+        let all_content = contents.reduce((all, content) => all + content);
         Object.keys(tags).forEach((tag) => {
             keywords = tags[tag];
-            if (keywords.filter(keyword => all_content.toLowerCase().includes(keyword)).length > 0) {
+            if (keywords.filter(keyword => all_content.includes(keyword)).length > 0) {
                 new_tags.push(tag)
             }
         });
@@ -68,24 +68,24 @@ function check_sayit_content(old_tags, contents) {
     return new_tags;
 }
 
-function check_title(old_tags, topic) {
+function check_title(old_tags) {
 
     new_tags = old_tags.slice();
 
     Object.keys(tags).forEach((tag) => {
         keywords = tags[tag];
-        if (keywords.filter(keyword => topic.title.toLowerCase().includes(keyword)).length > 0) {
-            new_tags.push(tag)
+        if (keywords.filter(keyword => topic.title.includes(keyword)).length > 0) {
+            new_tags.push(tag);
         }
     })
 
     return new_tags;
 }
 
-function get_sayit_link(topic) {
+function get_sayit_link() {
     return axios.get("https://talk.pdis.nat.gov.tw/t/" + topic.id + ".json")
         .then(body => {
-            links = [];
+            let links = [];
             body.data.details.links
                 .filter(link => link.url.includes("sayit"))
                 .map(link => links.push(link.url));
@@ -94,7 +94,7 @@ function get_sayit_link(topic) {
 }
 
 function get_sayit_content(links) {
-    promises = [];
+    let promises = [];
     links.map(link => promises.push(axios.get(link + ".an")))
     return Promise.all(promises)
         .then(function (contents) {
@@ -102,7 +102,7 @@ function get_sayit_content(links) {
         });
 }
 
-function update_discourse(new_tags) {
+function update_discourse() {
     if (new_tags.some(val => topic_tags.indexOf(val) === -1)) {
         return axios.put(
             'https://talk.pdis.nat.gov.tw/t/' + topic_slug + '/' + topic_id + '?api_key=' + apikey + '&api_user=' + apiuser,
@@ -110,8 +110,6 @@ function update_discourse(new_tags) {
         .then(body => body.data)
         .catch(err => err);
     }
-    else{
-      return Promise.resolve(tags)
-    }
+    return Promise.resolve(tags);
 }
 
